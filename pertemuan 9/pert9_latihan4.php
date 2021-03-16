e8def9602986e6acb9e70480982f5b4895659dba
danisaputra0306 <79980319+danisaputra0306@users.noreply.github.com>
2021-03-17 01:33:46 +0700

    Add files via upload

---
+++ pertemuan 9/pert9_latihan4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tanggal</title>
+	<style type="text/css">
+		h3	{
+			color: Grren;
+		}
+	</style>
+</head>
+<body>
+	<font size="10px">
+		<h3>Fungsi Date</h3>
+		<h4>By Dani Saputra</h4>
+	<?php 
+	echo "Date Now :";
+	echo date('d-F-Y');
+	echo "<br> dan jam ";
+	echo date('h:i:s A');
+	 ?>
+	</font>
+</body>
+</html>
